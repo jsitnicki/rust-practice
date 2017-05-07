afce7710db9479a89755e42ee56dac7a497e5498
Jakub Sitnicki <jsitnicki@gmail.com>
2017-05-07 13:44:01 +0200

    Print a heading above the table

--- fahr.rs
+++ fahr.rs
@@ -7,6 +7,9 @@ fn main() {
     let upper: f32 = 120.0;
     let step:  f32 = 5.0;
 
+    println!("{:>3}\t{:>6}", "°F", "°C");
+    println!("{:->3}\t{:->6}", "", "");
+
     fahr = lower;
     while fahr <= upper {
         cels = (5.0/9.0) * (fahr-32.0);
