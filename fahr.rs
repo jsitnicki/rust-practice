a921cb1854f2485084804017b6ee22764af411fe
Jakub Sitnicki <jsitnicki@gmail.com>
2017-05-07 13:44:01 +0200

    Switch to floating-point arithmetic

--- fahr.rs
+++ fahr.rs
@@ -1,15 +1,16 @@
 /* Print Fahrenheit-Celsius table */
 fn main() {
-    let mut fahr;
-    let mut cels;
-    let lower = 0;
-    let upper = 120;
-    let step  = 5;
+    let mut fahr: f32;
+    let mut cels: f32;
+
+    let lower: f32 = 0.0;
+    let upper: f32 = 120.0;
+    let step:  f32 = 5.0;
 
     fahr = lower;
     while fahr <= upper {
-        cels = 5 * (fahr-32) / 9;
-        println!("{}\t{}", fahr, cels);
+        cels = (5.0/9.0) * (fahr-32.0);
+        println!("{:3.0}\t{:6.1}", fahr, cels);
         fahr = fahr + step;
     }
 }
